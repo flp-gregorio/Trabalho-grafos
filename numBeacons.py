fb7ba9e7895f2176202ba35a55d7879f96dc8739
LeandroRochAlg <leandrochaju@hotmail.com>
2023-06-26 21:08:13 -0300

    Adição de beacons finalizada e testada

--- numBeacons.py
+++ numBeacons.py
@@ -1,20 +1,15 @@
-alcance = 80 #alcance do beacon em metros
-coberturaMin = 3 #cobertura mínima de beacons necessária em um ponto
-cont = [1] #contador de beacons
-cont[0] = 0
+import math
 
 def posicionaBeacons(matriz, alcance, coberturaMin):
     cont = [1] #contador de beacons
     cont[0] = 0
 
     def calculaBeacons(matriz):
-        beacons = [[-1] * len(matriz[0]) for i in range(len(matriz))]  # matriz de beacons, -1 = não possui beacon, 0, 1, 2, 3, ... = id do beacon
-        
         for i in range(len(matriz)):
             for j in range(len(matriz)):
                 if matriz[i][j] == -1:  #não faz parte da planta
                     continue 
-                while matriz[i][j] <= coberturaMin: #não tem cobertura de 3 beacons
+                while matriz[i][j] < coberturaMin: #não tem cobertura de 3 beacons
                     adicionaBeacon(matriz, i, j, beacons)
 
         return beacons
@@ -22,7 +17,7 @@ def posicionaBeacons(matriz, alcance, coberturaMin):
     def adicionaBeacon(matriz, i, j, beacons):
         #a ideia do código é achar a posição do beacon que gerará a maior cobertura possível
         
-        cobertura = 0
+        cobertura = -math.inf
 
         #calcula a melhor posição para o beacon dentro do raio do ponto analisado
         for x in range(-alcance, alcance+1):
@@ -30,9 +25,10 @@ def posicionaBeacons(matriz, alcance, coberturaMin):
                 if x**2 + y**2 <= alcance**2:  # verifica se a posição (x, y) está dentro do alcance circular
                     if i+x < 0 or i+x >= len(matriz) or j+y < 0 or j+y >= len(matriz[0]):  # verifica se a posição está dentro da matriz
                         continue
-                    elif matriz[i+x][j+y] != -1 and beacons[i+x][j+y] == 0: # verifica se a posição não é um obstáculo e não possui um beacon
-                        if calculaCobertura(matriz, i+x, j+y, beacons) > cobertura: #verifica se a cobertura é maior que a anterior
-                            cobertura = calculaCobertura(matriz, i+x, j+y, beacons)
+                    elif matriz[i+x][j+y] != -1 and beacons[i+x][j+y] == -1: # verifica se a posição não é um obstáculo e não possui um beacon
+                        coberturaCalculada = calculaCobertura(matriz, i+x, j+y, beacons) #calcula a cobertura da posição (x, y)
+                        if coberturaCalculada > cobertura: #verifica se a cobertura é maior que a anterior
+                            cobertura = coberturaCalculada
                             posX = i+x
                             posY = j+y
 
@@ -45,7 +41,7 @@ def posicionaBeacons(matriz, alcance, coberturaMin):
                 if x**2 + y**2 <= alcance**2:
                     if posX+x < 0 or posX+x >= len(matriz) or posY+y < 0 or posY+y >= len(matriz[0]):
                         continue
-                    elif matriz[posX+x][posY+y] != -1 and beacons[posX+x][posY+y] == 0:
+                    elif matriz[posX+x][posY+y] != -1:
                         matriz[posX+x][posY+y] += 1
 
     def calculaCobertura(matriz, i, j, beacons):
@@ -56,11 +52,36 @@ def posicionaBeacons(matriz, alcance, coberturaMin):
                 if x**2 + y**2 <= alcance**2:  # verifica se a posição (x, y) está dentro do alcance circular
                     if i+x < 0 or i+x >= len(matriz) or j+y < 0 or j+y >= len(matriz[0]):  # verifica se a posição está dentro da matriz
                         continue
-                    elif matriz[i+x][j+y] != -1 and beacons[i+x][j+y] == 0:  # verifica se a posição não é um obstáculo e não possui um beacon
+                    elif matriz[i+x][j+y] != -1 and beacons[i+x][j+y] == -1:  # verifica se a posição não é um obstáculo e não possui um beacon
                         cobertura += coberturaMin - matriz[i+x][j+y]  # adiciona a cobertura do beacon na posição (x, y) com prioridade para os pontos com menor cobertura
-        
+
         return cobertura
     
     return calculaBeacons(matriz)
+
+def printar_matriz(matriz):
+    for linha in matriz:
+        for elemento in linha:
+            print(elemento, end=' ')
+        print()  # Pula para a próxima linha após imprimir todos os elementos de uma linha
     
+import random
+
+x = 800  # número de linhas
+y = 600  # número de colunas
+num_minus_ones = 43200  # quantidade de elementos -1
+
+matriz = [[0] * y for _ in range(x)]
+
+for _ in range(num_minus_ones):
+    i = random.randint(0, x - 1)
+    j = random.randint(0, y - 1)
+    matriz[i][j] = -1
+
+beacons = [[-1] * len(matriz[0]) for i in range(len(matriz))]  # matriz de beacons, -1 = não possui beacon, 0, 1, 2, 3, ... = id do beacon
+
 posicionaBeacons(matriz, 80, 3)
+
+printar_matriz(matriz)
+print()
+printar_matriz(beacons)

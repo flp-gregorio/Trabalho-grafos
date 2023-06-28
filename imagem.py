11906652644966cbfa393c6945070b430250129d
LeandroRochAlg <leandrochaju@hotmail.com>
2023-06-27 21:50:20 -0300

    Transformação da imagem da planta para matriz

---
+++ imagem.py
@@ -0,0 +1,29 @@
+from PIL import Image
+
+def image_to_matrix(image_path, width, height):
+    # Carrega a imagem em escala de cinza
+    image = Image.open(image_path).convert('L')
+
+    # Redimensiona a imagem para o tamanho desejado
+    image = image.resize((width, height))
+
+    # Obtém os pixels da imagem como uma lista de valores
+    pixels = list(image.getdata())
+
+    # Cria a matriz com base nos pixels da imagem
+    matrix = []
+    for i in range(0, len(pixels), width):
+        row = pixels[i:i+width]
+        matrix.append([-1 if pixel == 255 else 0 for pixel in row])
+
+    return matrix
+
+# Exemplo de uso
+image_path = 'planta_com_vazios.png'
+width = 72
+height = 64
+matrix = image_to_matrix(image_path, width, height)
+
+# Imprime a matriz resultante
+for row in matrix:
+    print(row)

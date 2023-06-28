c48430dee1250189b060f6b5eeb5865c959dd810
LeandroRochAlg <leandrochaju@hotmail.com>
2023-06-28 12:54:38 -0300

    vamo usá 42

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import numpy as np
 
 if not os.path.isfile("Trabalho-grafos/beacons.pickle"):
     matriz = im.image_to_matrix('Trabalho-grafos/planta_com_vazios.png', 72, 64)
-    beacons = bea.Beacons(matriz, 40, 3)
+    beacons = bea.Beacons(matriz, 42, 3)
     beacons.calculaBeacons()
     beacons.getListaAdjacencias()
     with open("Trabalho-grafos/beacons.pickle", "wb") as f:

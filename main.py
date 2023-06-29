8c41aa4e390b825ca739222060b4cbcc31b3ae6c
LeandroRochAlg <leandrochaju@hotmail.com>
2023-06-28 21:54:16 -0300

    Opções de plot

--- main.py
+++ main.py
@@ -21,52 +21,105 @@ else:
     with open("Trabalho-grafos/beacons.pickle", "rb") as f:
         beacons = pickle.load(f)
 
-dataset = pd.read_csv('Trabalho-grafos/coords.csv')  # Extrair as informações dos beacons
+def plotarImagem(opcao1, opcao2):
+    dataset = pd.read_csv('Trabalho-grafos/coords.csv')  # Extrair as informações dos beacons
 
-# Carregar a imagem png como plano de fundo
-plano_fundo = mpimg.imread(r'Trabalho-grafos/planta_com_vazios.png')
+    # Carregar a imagem png como plano de fundo
+    plano_fundo = mpimg.imread(r'Trabalho-grafos/planta_com_vazios.png')
 
-# Criar o plano cartesiano
-fig, ax = plt.subplots()
+    # Criar o plano cartesiano
+    fig, ax = plt.subplots()
 
-# Definir a imagem JPEG como plano de fundo
-ax.imshow(plano_fundo, extent=[0, 72, 64, 0])  # Definir os limites do plano cartesiano
+    # Definir a imagem JPEG como plano de fundo
+    ax.imshow(plano_fundo, extent=[0, 72, 64, 0])  # Definir os limites do plano cartesiano
 
-pontos, listaAdjPontos, matrizAdjPontos = proc.criar_pontos(dataset)  # Extrair as informações dos pontos
+    pontos, listaAdjPontos, matrizAdjPontos = proc.criar_pontos(dataset)  # Extrair as informações dos pontos
 
-for ponto, info in pontos.items():
-    nome = info['nome']
-    coordenadas = info['coordenadas']
-    x, y = coordenadas
-    ax.plot(x, y, 'ro')  # 'ro' representa o ponto vermelho
-    ax.annotate(nome, (x, y), xytext=(5, 5), textcoords='offset points')
+    for ponto, info in pontos.items():
+        nome = info['nome']
+        coordenadas = info['coordenadas']
+        x, y = coordenadas
+        ax.plot(x, y, 'ro')  # 'ro' representa o ponto vermelho
+        ax.annotate(nome, (x, y), xytext=(5, 5), textcoords='offset points')
 
-for vertice in listaAdjPontos:
-    for adj in listaAdjPontos[vertice]:
-        for ponto, info in pontos.items():
-            if info['id'] == vertice:
-                x1, y1 = info['coordenadas']
-            if info['id'] == adj:
-                x2, y2 = info['coordenadas']
-        ax.plot([x1, x2], [y1, y2], 'b-')   # 'b-' representa a linha azul
+    for vertice in listaAdjPontos:
+        for adj in listaAdjPontos[vertice]:
+            for ponto, info in pontos.items():
+                if info['id'] == vertice:
+                    x1, y1 = info['coordenadas']
+                if info['id'] == adj:
+                    x2, y2 = info['coordenadas']
+            ax.plot([x1, x2], [y1, y2], 'b-')   # 'b-' representa a linha azul
 
-# Plotar os beacons e suas conexões
-for i in range(len(beacons.beacons)):
-    for j in range(len(beacons.beacons[0])):
-        if beacons.beacons[i][j] != -1:
-            ax.plot(j, i, 'go')  # 'go' representa o beacon azul
-            ax.annotate(beacons.beacons[i][j], (j, i), xytext=(5, 5), textcoords='offset points')
+    if opcao1 == 1:
+        # Plotar os beacons e suas conexões
+        for i in range(len(beacons.beacons)):
+            for j in range(len(beacons.beacons[0])):
+                if beacons.beacons[i][j] != -1:
+                    ax.plot(j, i, 'go')  # 'go' representa o beacon azul
+                    ax.annotate(beacons.beacons[i][j], (j, i), xytext=(5, 5), textcoords='offset points')
 
-            for adj in beacons.listaAdjacencias[beacons.beacons[i][j]]:
-                for m in range(len(beacons.beacons)):
-                    for n in range(len(beacons.beacons[0])):
-                        if beacons.beacons[m][n] == adj:
-                            ax.plot([j, n], [i, m], 'g--')  # 'g--' representa a conexão entre beacons em linha tracejada
+                    for adj in beacons.listaAdjacencias[beacons.beacons[i][j]]:
+                        for m in range(len(beacons.beacons)):
+                            for n in range(len(beacons.beacons[0])):
+                                if beacons.beacons[m][n] == adj:
+                                    ax.plot([j, n], [i, m], 'g--')  # 'g--' representa a conexão entre beacons em linha tracejada
 
-            # Adicionar o círculo em torno do beacon
-            circle = plt.Circle((j, i), radius=alcance, color='g', fill=False)
-            ax.add_artist(circle)
+                    if opcao2 == 1:
+                        # Adicionar o círculo em torno do beacon
+                        circle = plt.Circle((j, i), radius=alcance, color='g', fill=False)
+                        ax.add_artist(circle)
 
-plt.grid(True, color='grey', linewidth=0.5) # Adicionar a grade ao plano cartesiano
+    plt.grid(True, color='grey', linewidth=0.5) # Adicionar a grade ao plano cartesiano
 
-plt.show()  # Mostrar o plano cartesiano
+    plt.show()  # Mostrar o plano cartesiano
+
+if __name__ == "__main__":
+    op = 0
+    op1 = 0
+    op2 = 0
+
+
+    while 1:
+        print("Selecione a ação desejada:")
+        print("1 - Plotar imagem")
+        print("2 - Simular")
+        print("3 - Sair")
+
+        op = int(input())
+        
+        if op == 1:
+            while 1:
+                print("Com beacons ou sem beacons?")
+                print("1 - Com beacons")
+                print("2 - Sem beacons")
+                
+                op1 = int(input())
+
+                if op1 == 1:
+                    print("Com ou sem raio de alcance?")
+                    print("1 - Com raio de alcance")
+                    print("2 - Sem raio de alcance")
+                    
+                    op2 = int(input())
+
+                    if op2 == 1:
+                        break
+                    elif op2 == 2:
+                        break
+                    else:
+                        print("Opção inválida")
+                        continue
+                elif op1 == 2:
+                    break
+                else:
+                    print("Opção inválida")
+                    continue
+            plotarImagem(op1, op2)
+        elif op == 2:
+            pass
+        elif op == 3:
+            break
+        else:
+            print("Opção inválida")
+            continue
